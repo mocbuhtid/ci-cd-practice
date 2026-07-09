3d2a9cf37864da24c518e65391fe90648e514d37
mocbuhtid <d0u0.jp@gmail.com>
2026-07-09 13:22:41 +0900

    Add multiply function

--- test_calc.py
+++ test_calc.py
@@ -1,4 +1,4 @@
-from calc import add, subtract
+from calc import add, subtract, multiply
 
 
 def test_add():
@@ -7,3 +7,7 @@ def test_add():
 
 def test_subtract():
     assert subtract(5, 3) == 2
+
+
+def test_multiply():
+    assert multiply(2, 3) == 6
